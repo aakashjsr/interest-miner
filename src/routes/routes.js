9dab61eff5e94d084e0f5873e5da362f8a4136d9
Sohaib Raza <sohaibraza789@gmail.com>
2020-04-14 00:14:12 +0400

    Add Paper & View Paper

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,9 @@ import Profile from "../views/examples/Profile.js";
 import Maps from "../views/examples/Maps.js";
 import Register from "../views/Register.js";
 import Login from "../views/Login.js";
+import AddPaper from "../views/AddPaper.js";
+import ViewPaper from "../views/ViewPaper.js";
+
 import Tables from "../views/examples/Tables.js";
 import Icons from "../views/examples/Icons.js";
 
@@ -16,18 +19,30 @@ var routes = [
     layout: "/admin"
   },
   {
-    path: "/icons",
-    name: "Icons",
-    icon: "ni ni-planet text-blue",
-    component: Icons,
-    layout: "/admin"
+    path: "/add-paper",
+    name: "Add Paper",
+    icon: "fas fa-tasks text-blue",
+    component: AddPaper,
+    layout: "/admin",
+    display: "none"
   },
+   {
+    path: "/view-paper",
+    name: "View Paper",
+    icon: "fas fa-tasks text-blue",
+    component: ViewPaper,
+    layout: "/admin",
+    display: "none"
+  },
+  
   {
     path: "/maps",
     name: "Maps",
     icon: "ni ni-pin-3 text-orange",
     component: Maps,
-    layout: "/admin"
+    layout: "/admin",
+    display: "none"
+
   },
   {
     path: "/user-profile",
@@ -41,21 +56,26 @@ var routes = [
     name: "Tables",
     icon: "ni ni-bullet-list-67 text-red",
     component: Tables,
-    layout: "/admin"
+    layout: "/admin",
+    display: "none"
+
   },
   {
     path: "/login",
     name: "Login",
     icon: "ni ni-key-25 text-info",
     component: Login,
-    layout: "/auth"
+    layout: "/auth",
+    display: "none"
   },
   {
     path: "/register",
     name: "Register",
     icon: "ni ni-circle-08 text-pink",
     component: Register,
-    layout: "/auth"
+    layout: "/auth",
+    display: "none"
+    
   }
 ];
 export default routes;
---
+++ src/views/AddPaper.js
@@ -0,0 +1,295 @@
+
+import React from "react";
+// react plugin used to create google maps
+import { toast } from 'react-toastify';
+import Loader from 'react-loader-spinner'
+import { handleServerErrors } from "utils/errorHandler";
+import user from '../services/api';
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
+
+// core components
+import Header from "components/Headers/Header.js";
+
+
+class AddPaper extends React.Component {
+
+  state = {
+    title: '',
+    url:'',
+    year:'',
+    abstract:''
+  }
+
+  handleChange = e => {
+    let getValue = e.target.value;
+    let getName = e.target.name;
+    this.setState(() => ({ [getName]: getValue }))
+  };
+
+  _handleSubmit = e => {
+    e.preventDefault();
+      let data = {
+        title: this.state.title,
+        url: this.state.url,
+        year: this.state.year,
+        abstract: this.state.abstract,
+      };
+      
+      this.setState({ isLoding: true })
+      // loder true ka code 
+      // user.userSignup(data).then(response => {
+      //   // loder false ka code 
+      //   this.setState({ isLoding: false })
+      //   this.props.history.push("/auth/login");
+
+      // }
+      // ).catch(error => {
+      //   this.setState({ isLoding: false })
+      //   console.log(error)
+        
+      // })
+    
+  };
+
+
+  render() {
+    return (
+      <>
+        <Header />
+        {/* Page content */}
+        <Container className="mt--7" fluid>
+          <Row>
+            {/* <div className="col">
+              <Card className="shadow border-0">
+              Add Form
+              
+              </Card>
+            </div> */}
+             <Col className="order-xl-1" xl="12">
+              <Card className="bg-secondary shadow">
+                <CardHeader className="bg-white border-0">
+                  <Row className="align-items-center">
+                    <Col xs="8">
+                      <h3 className="mb-0">Add Paper</h3>
+                    </Col>
+                    {/* <Col className="text-right" xs="4">
+                      <Button
+                        color="primary"
+                        href="#pablo"
+                        onClick={e => e.preventDefault()}
+                        size="sm"
+                      >
+                        Settings
+                      </Button>
+                    </Col> */}
+                  </Row>
+                </CardHeader>
+                <CardBody>
+                  <Form onSubmit={this._handleSubmit} method="post">
+                    <h6 className="heading-small text-muted mb-4">
+                      Paper information
+                    </h6>
+                    <div className="pl-lg-4">
+                      <Row>
+                        <Col lg="6">
+                          <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-username"
+                            >
+                              Title
+                            </label>
+                            <Input
+                              className="form-control-alternative"
+                              // defaultValue="lucky.jesse"
+                              id="input-username"
+                              name="title" value={this.state.title} onChange={this.handleChange} 
+                              placeholder="Title"
+                              type="text"
+                            />
+                          </FormGroup>
+                        </Col>
+                        <Col lg="6">
+                          <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-email"
+                            >
+                              URL
+                            </label>
+                            <Input
+                              className="form-control-alternative"
+                              id="input-email"
+                              name="url" value={this.state.url} onChange={this.handleChange} 
+                              placeholder="https://www.zyz.com"
+                              type="text"
+                            />
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col lg="6">
+                          <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-first-name"
+                            >
+                              Year
+                            </label>
+                            <Input
+                              className="form-control-alternative"
+                              defaultValue="Lucky"
+                              id="input-first-name"
+                              name="year" value={this.state.year} onChange={this.handleChange} 
+                              placeholder="Year"
+                              type="number"
+                            />
+                          </FormGroup>
+                        </Col>
+                        <Col lg="6">
+                          <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-last-name"
+                            >
+                              Abstract
+                            </label>
+                            <Input
+                              className="form-control-alternative"
+                              // defaultValue="Jesse"
+                              id="input-last-name"
+                              name="abstract" value={this.state.abstract} onChange={this.handleChange} 
+                              placeholder="Abstract"
+                              type="textarea"
+                            />
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                    </div>
+                    {/* <hr className="my-4" /> */}
+                    {/* Address */}
+                    {/* <h6 className="heading-small text-muted mb-4">
+                      Contact information
+                    </h6>
+                    <div className="pl-lg-4">
+                      <Row>
+                        <Col md="12">
+                          <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-address"
+                            >
+                              Address
+                            </label>
+                            <Input
+                              className="form-control-alternative"
+                              defaultValue="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                              id="input-address"
+                              placeholder="Home Address"
+                              type="text"
+                            />
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <Col lg="4">
+                          <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-city"
+                            >
+                              City
+                            </label>
+                            <Input
+                              className="form-control-alternative"
+                              defaultValue="New York"
+                              id="input-city"
+                              placeholder="City"
+                              type="text"
+                            />
+                          </FormGroup>
+                        </Col>
+                        <Col lg="4">
+                          <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-country"
+                            >
+                              Country
+                            </label>
+                            <Input
+                              className="form-control-alternative"
+                              defaultValue="United States"
+                              id="input-country"
+                              placeholder="Country"
+                              type="text"
+                            />
+                          </FormGroup>
+                        </Col>
+                        <Col lg="4">
+                          <FormGroup>
+                            <label
+                              className="form-control-label"
+                              htmlFor="input-country"
+                            >
+                              Postal code
+                            </label>
+                            <Input
+                              className="form-control-alternative"
+                              id="input-postal-code"
+                              placeholder="Postal code"
+                              type="number"
+                            />
+                          </FormGroup>
+                        </Col>
+                      </Row>
+                    </div> */}
+                    <hr className="my-4" />
+                    {/* Description */}
+                    {/* <h6 className="heading-small text-muted mb-4">About me</h6>
+                    <div className="pl-lg-4">
+                      <FormGroup>
+                        <label>About Me</label>
+                        <Input
+                          className="form-control-alternative"
+                          placeholder="A few words about you ..."
+                          rows="4"
+                          defaultValue="A beautiful Dashboard for Bootstrap 4. It is Free and
+                          Open Source."
+                          type="textarea"
+                        />
+                      </FormGroup>
+                    </div> */}
+                     <Button
+                        color="primary"
+                        type="submit"
+                        // onClick={e => e.preventDefault()}
+                        // size="md"
+                      >
+                        Save
+                      </Button>
+                  </Form>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default AddPaper;
