55e45366ddd521afa08ac3a17f6ca2a02c872f53
kusumkumari <kusumk2992@gmail.com>
2020-07-17 21:31:42 +0530

    change

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://ec2-3-94-89-46.compute-1.amazonaws.com:8000";
+export const BASE_URL = "http://ec2-54-80-106-180.compute-1.amazonaws.com:8000";
 export const TOKEN = "cdf895267756d954747b63b9cc56e23fa06ecd29";
