cacc8fd150099e22280d552f87deba601e170c99
kusum <kusumk2992@gmail.com>
2020-07-18 00:32:07 +0530

    Venn Diagram Ui Creation (#50)
    Co-authored-by: vivekprasadebeta <vivek@ebetainnovations.com>

---
+++ src/components/UserCharts/VennDiagram.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import { Modal, ModalBody, ModalFooter, Button } from "reactstrap";
+
+class VennDiagram extends Component {
+  state = {
+    modal: false,
+  };
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal,
+    });
+  };
+  open = () => {
+    this.setState({
+      modal: true,
+    });
+  };
+  render() {
+    return (
+      <>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            border: "1px solid gainsboro",
+            borderTopLeftRadius: "8px",
+            borderTopRightRadius: "8px",
+          }}
+        >
+          <div
+            style={{
+              width: "35%",
+              marginRight: "15%",
+              textAlign: "center",
+              padding: "10px",
+            }}
+          >
+            Word1
+          </div>
+          <div
+            style={{
+              width: "35%",
+              marginLeft: "15%",
+              textAlign: "center",
+              padding: "10px",
+            }}
+          >
+            Word1
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          <div
+            style={{
+              width: "40%",
+              borderBottomLeftRadius: "8px",
+              background: "rgba(50, 151, 211, 0.18)",
+              borderRight: "1px solid ghostwhite",
+            }}
+          >
+            <p style={{ textAlign: "center" }}>Word1</p>
+            <p style={{ textAlign: "center" }}>Word1</p>
+            <p style={{ textAlign: "center" }}>Word1</p>
+            <p style={{ textAlign: "center" }}>Word1</p>
+          </div>
+          <div
+            style={{
+              width: "30%",
+              textAlign: "center",
+              background: "rgba(50, 151, 211, 0.18)",
+            }}
+          >
+            <Button
+              color="primary"
+              onClick={this.open}
+              style={{ transform: "translateY(-50%)", top: "50%" }}
+            >
+              Similar
+            </Button>
+          </div>
+          <div
+            style={{
+              width: "40%",
+              borderBottomRightRadius: "8px",
+              borderLeft: "1px solid ghostwhite",
+              background: "rgba(50, 151, 211, 0.18)",
+            }}
+          >
+            <p style={{ textAlign: "center" }}>Word1</p>
+            <p style={{ textAlign: "center" }}>Word1</p>
+            <p style={{ textAlign: "center" }}>Word1</p>
+            <p style={{ textAlign: "center" }}>Word1</p>
+          </div>
+        </div>
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          size="lg"
+          id="modal"
+        >
+          <ModalBody>
+            <h3>Similar Keywords</h3>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>
+              OK
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </>
+    );
+  }
+}
+export default VennDiagram;
